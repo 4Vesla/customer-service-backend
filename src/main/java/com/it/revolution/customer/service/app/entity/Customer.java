50cef2cfb13f8aa5df98076cb8921d57563f9aa6
Roman_Kovalchuk <Roman_Kovalchuk@epam.com>
2022-07-16 16:18:17 +0300

    Added authorized roles and some addition attributes

--- src/main/java/com/it/revolution/customer/service/app/entity/Customer.java
+++ src/main/java/com/it/revolution/customer/service/app/entity/Customer.java
@@ -2,14 +2,18 @@ package com.it.revolution.customer.service.app.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
-public class Customer {
+public class Customer implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,7 +28,51 @@ public class Customer {
     @Column(name = "phone_number")
     private String phoneNumber;
 
+    @Column(name = "email")
+    private String email;
+
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "activation_code")
+    private String activationCode;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
---
+++ src/main/java/com/it/revolution/customer/service/app/entity/Role.java
@@ -0,0 +1,13 @@
+package com.it.revolution.customer.service.app.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+
+    ADMIN, USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
