5ec81ad84d63b22f69e3c2b46b6de8a2deb94eaa
Roman_Kovalchuk <Roman_Kovalchuk@epam.com>
2022-07-16 19:08:24 +0300

    Configured web security config

--- src/main/java/com/it/revolution/customer/service/app/config/WebSecurityConfig.java
+++ src/main/java/com/it/revolution/customer/service/app/config/WebSecurityConfig.java
@@ -1,16 +1,24 @@
 package com.it.revolution.customer.service.app.config;
 
+import com.it.revolution.customer.service.app.service.CustomerService;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@AllArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomerService customerService;
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -23,12 +31,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login").permitAll()
             .and()
                 .logout().permitAll()
-            .and().csrf().disable();
+            .and().csrf().disable()
+        ;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //TODO authentication
+        auth.userDetailsService(customerService).passwordEncoder(passwordEncoder);
     }
 
 }
