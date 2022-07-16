81df3be491b9eb2d1fbcc3368d115765aae3b24e
Roman_Kovalchuk <Roman_Kovalchuk@epam.com>
2022-07-16 16:28:56 +0300

    added web security config

---
+++ src/main/java/com/it/revolution/customer/service/app/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.it.revolution.customer.service.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/users", "/user/**", "/login", "/registration")
+                .authenticated()
+            .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+            .and()
+                .logout().permitAll()
+            .and().csrf().disable();
+    }
+
+
+}
