c3a13a61ef957d10597da39f8ad62c67516c9e83
Roman_Kovalchuk <Roman_Kovalchuk@epam.com>
2022-07-18 18:45:03 +0300

    Disabled cors

--- src/main/java/com/it/revolution/customer/service/app/config/WebSecurityConfig.java
+++ src/main/java/com/it/revolution/customer/service/app/config/WebSecurityConfig.java
@@ -40,6 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .httpBasic().disable()
                 .csrf().disable()
+                .cors().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
                 .authorizeRequests()
