01f4c88bf8644ff50b972cca37fe60d7a2f0b1a4
Ruslan Humeniuk <ruslan21343@gmail.com>
2022-07-18 11:16:33 +0300

    Minor settings improvement

--- src/main/java/com/it/revolution/customer/service/app/amazon/settings/AWSS3Settings.java
+++ src/main/java/com/it/revolution/customer/service/app/amazon/settings/AWSS3Settings.java
@@ -1,13 +1,16 @@
 package com.it.revolution.customer.service.app.amazon.settings;
 
+import com.it.revolution.customer.service.app.common.AbstractSettings;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import javax.validation.constraints.NotNull;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @ConfigurationProperties(prefix = "amazon.s3")
-public class AWSS3Settings {
+public class AWSS3Settings extends AbstractSettings {
 
     @NotNull
     private Bucket bucket;
